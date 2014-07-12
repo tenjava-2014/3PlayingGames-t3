d81b8e9b0fd75732518f1570b985c9f6ac99a0f2
Nick <nick@nickparksdev.com>
2014-07-12 13:29:13 -0700

    Potato~

--- src/main/java/com/tenjava/entries/3PlayingGames/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/3PlayingGames/t3/TenJava.java
@@ -1,5 +1,14 @@
 package com.tenjava.entries.p3PlayingGames.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.ConsoleCommandSender;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+  @Override
+  public void onEnable(){
+    //I had no ideas for a plugin so this is the best I could do <3
+    ConsoleCommandSender consoleCommandSender = plugin.getServer().getConsoleSender();
+    consoleCommandSender.sendMessage(ChatColor.RED + "Potato~");
+  }
+}
